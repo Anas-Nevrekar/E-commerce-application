a20015bbcf4c454b95dd9c2eb262d290e6203268
Anas Nevrekar <nevrekaranas7@gmail.com>
2025-08-14 20:19:47 +0530

    Implement encryption of password

--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.model'); // Importing the User model to interact with the database
 const {authMiddleware, generateToken} = require('../middlewares/jwt.middleware'); // Importing the JWT middleware for authentication
-
+const bcrypt = require('bcrypt'); // Importing bcrypt for password hashing
 
 // Render the login view using EJS
 exports.showLoginPage = async (req, res) => {
@@ -9,14 +9,18 @@ exports.showLoginPage = async (req, res) => {
 
 // logic to implement login feature 
 exports.handleLogin = async (req, res) => {
-  const { username, password } = req.body;
+   const username = req.body.username;
+   const password = req.body.password.trim();
   try {
       const user = await User.findOne({ username: username });
       if (!user) {
+        console.log("No such user");
           return res.redirect("/loginPage");
       }
       //const isMatch= await User.comparePassword(password);
-      if (user.password !== password) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        console.log("Incorrect password");
           return res.redirect("/loginPage");
       }
       const token = generateToken({ username: username });
@@ -31,7 +35,11 @@ exports.handleLogin = async (req, res) => {
 
 // Logic to handle user sign-in
 exports.handleSignin = async (req, res) => {
-  const { username, email, password } = req.body;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password.trim();
+
+  //const { username, email, password } = req.body;
   try
   {
       const ExistingUser = await User.findOne({ email: email, username: username });
@@ -42,10 +50,11 @@ exports.handleSignin = async (req, res) => {
         }
       else
         {
+          const hash_password = await bcrypt.hash(password, 10); // Hash the password using bcrypt
           const newUser = new User({
               username: username,
               email: email,
-              password: password
+              password: hash_password
           });
           await newUser.save(); // Save the new user to the database
           const token = generateToken({ username: username}); // Generate a JWT token for the user
